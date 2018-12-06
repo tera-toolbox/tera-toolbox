81e69a03771251b6bac91cf562d0935423283086
Caali <modsfortera@gmail.com>
2018-12-06 17:41:27 +0100

    Enforce strict mode.

--- TeraProxy.bat
+++ TeraProxy.bat
@@ -1,4 +1,5 @@
 @echo off
+title Caali's TERA Proxy
 @setlocal enableextensions
 @cd /d "%~dp0/bin/lib"
 
@@ -6,8 +7,9 @@ WHERE node > NUL 2> NUL
 IF %ERRORLEVEL% NEQ 0 (
   ECHO ERROR: Node.js is not installed!
   ECHO ERROR: Please go to https://discord.gg/dUNDDtw and follow the installation guide.
-  ECHO(
-  PAUSE
 ) ELSE (
-  START cmd.exe /k "node index.js"
+  node --use-strict index.js
 )
+
+ECHO(
+PAUSE
