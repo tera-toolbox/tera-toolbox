27dfa985409af84b1f868d63e60b8898c789cc5d
Caali <modsfortera@gmail.com>
2018-12-06 17:55:51 +0100

    Improvements to proxy self-update.

--- bin/lib/update-self.js
+++ bin/lib/update-self.js
@@ -36,9 +36,16 @@ function forcedirSync(dir) {
   }, initDir);
 }
 
-async function autoUpdateFile(file, filepath, url) {
+function hash(data) {
+  return crypto.createHash("sha256").update(data).digest().toString("hex").toUpperCase();
+}
+
+async function autoUpdateFile(file, filepath, url, expectedHash = null) {
   try {
     const updatedFile = await request({url: url, encoding: null});
+
+    if(expectedHash && expectedHash !== hash(updatedFile))
+      throw new Error(`ERROR: ${url}\nDownloaded file doesn't match hash specified in patch manifest!`);
 
     forcedirSync(path.dirname(filepath));
     fs.writeFileSync(filepath, updatedFile);
@@ -54,26 +61,34 @@ async function autoUpdateSelf(updatelimit = true, serverIndex = 0) {
     console.error("ERROR: Please join %s and download the prepackaged release version from the #proxy channel!", DiscordURL);
     return Promise.reject("Request not installed");
   }
+  
+  if(serverIndex == 0)
+    console.log(`[update] Proxy self-update started (Branch: ${branch})`);
 
   try {
     const manifest = await request({url: TeraProxyAutoUpdateServers[serverIndex] + branch + '/manifest.json', json: true});
     if(!manifest["files"])
-      throw "Invalid manifest!";
+      throw new Error("Invalid manifest!");
 
     let promises = [];
     for(let file in manifest["files"]) {
       let filepath = path.join(__dirname, "..", "..", file);
       let filedata = manifest["files"][file];
       let needsUpdate = !fs.existsSync(filepath);
+      let expectedHash = null;
+
       if(!needsUpdate) {
         if(typeof filedata === 'object') {
-          needsUpdate = filedata["overwrite"] && (crypto.createHash("sha256").update(fs.readFileSync(filepath)).digest().toString("hex").toUpperCase() !== filedata["hash"].toUpperCase());
+          expectedHash = filedata["hash"].toUpperCase();
+          needsUpdate = filedata["overwrite"] && (hash(fs.readFileSync(filepath)) !== expectedHash);
         } else {
-          needsUpdate = (crypto.createHash("sha256").update(fs.readFileSync(filepath)).digest().toString("hex").toUpperCase() !== filedata.toUpperCase());
+          expectedHash = filedata.toUpperCase();
+          needsUpdate = (hash(fs.readFileSync(filepath)) !== expectedHash);
         }
       }
+
       if(needsUpdate) {
-        let promise = autoUpdateFile(file, filepath, TeraProxyAutoUpdateServers[serverIndex] + branch + '/' + file);
+        let promise = autoUpdateFile(file, filepath, TeraProxyAutoUpdateServers[serverIndex] + branch + '/' + file, expectedHash);
         promises.push(updatelimit ? (await promise) : promise);
       }
     }
@@ -97,10 +112,12 @@ async function autoUpdateSelf(updatelimit = true, serverIndex = 0) {
       return false;
     }
   } catch(e) {
+    console.error(`ERROR: Unable to self-update (Update server ${serverIndex}):\n${e}\n`);
     if(serverIndex + 1 < TeraProxyAutoUpdateServers.length) {
+      console.error('Attempting to use next update server...\n');
       return autoUpdateSelf(updatelimit, serverIndex + 1);
     } else {
-      console.error("ERROR: Unable to auto-update the proxy!: %s\nPlease join %s and check the #info and #help channels for further instructions.", e, DiscordURL);
+      console.error(`ERROR: Unable to perform self-update!:\n${e}\n\nPlease join ${DiscordURL} and check the #info and #help channels for further instructions.`);
       return Promise.reject(e);
     }
   }
