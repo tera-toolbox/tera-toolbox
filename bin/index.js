bbcaa7ee9e5e958f71d3ea6e66a86f86c3db7221
Caali <modsfortera@gmail.com>
2018-12-10 19:11:36 +0100

    What the fuck, git?

--- bin/index.js
+++ bin/index.js
@@ -1,15 +1,4 @@
 async function updateSelf() {
-  // Temporary code for migration to new file structure
-  try {
-      const fs = require('fs');
-      const path = require('path');
-      if(fs.existsSync(path.join(__dirname, '..', 'update-self.js'))) {
-        console.log('Proxy has undergone a major update.');
-        console.log('Please restart it to apply the changes!');
-        process.exit();
-      }
-  } catch(_) {}
-
   delete require.cache[require.resolve('./update-self')];
   const autoUpdateSelf = require("./update-self");
   try {
