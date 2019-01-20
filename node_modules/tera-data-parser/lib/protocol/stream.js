fc3aca8cf638d2f6c72882459f049931e583b9e2
Caali <modsfortera@gmail.com>
2019-01-20 04:13:13 +0100

    Dependency update.

--- node_modules/tera-data-parser/lib/protocol/stream.js
+++ node_modules/tera-data-parser/lib/protocol/stream.js
@@ -4,160 +4,163 @@ const log = require('../logger'),
       { Customize, SkillID, Vec3 } = require('./types')
 
 const MULT_INT16_TO_RAD = 1 / 0x8000 * Math.PI,
-	  MULT_RAD_TO_INT16 = 1 / Math.PI * 0x8000
+      MULT_RAD_TO_INT16 = 1 / Math.PI * 0x8000
 
 class Readable {
-	constructor(buffer, position = 0) {
-		this.buffer = buffer
-		this.position = position
-	}
+    constructor(buffer, position = 0) {
+        this.buffer = buffer
+        this.position = position
+    }
 
-	seek(n) { return this.position = n }
-	skip(n) { return this.position += n }
+    seek(n) { return this.position = n }
+    skip(n) { return this.position += n }
 
-	bool() {
-		const ret = this.byte()
-		if(ret > 1) log.debug(new Error('read byte not 0 or 1 for bool'))
-		return !!ret
-	}
+    bool() {
+        const ret = this.byte()
+        if(ret > 1) log.debug(new Error('read byte not 0 or 1 for bool'))
+        return !!ret
+    }
 
-	byte() { return this.buffer.readUInt8(this.position++) }
+    byte() { return this.buffer.readUInt8(this.position++) }
 
-	bytes(n) { return Buffer.from(this.buffer.slice(this.position, this.position += n)) }
+    bytes(n) { return Buffer.from(this.buffer.slice(this.position, this.position += n)) }
 
-	uint16() {
-		const ret = this.buffer.readUInt16LE(this.position)
-		this.position += 2
-		return ret
-	}
+    uint16() {
+        const ret = this.buffer.readUInt16LE(this.position)
+        this.position += 2
+        return ret
+    }
 
-	uint32() {
-		const ret = this.buffer.readUInt32LE(this.position)
-		this.position += 4
-		return ret
-	}
+    uint32() {
+        const ret = this.buffer.readUInt32LE(this.position)
+        this.position += 4
+        return ret
+    }
 
-	uint64() {
-		return BigInt(this.uint32()) | (BigInt(this.uint32()) << 32n)
-	}
+    uint64() {
+        return BigInt(this.uint32()) | (BigInt(this.uint32()) << 32n)
+    }
 
-	int16() {
-		const ret = this.buffer.readInt16LE(this.position)
-		this.position += 2
-		return ret
-	}
+    int16() {
+        const ret = this.buffer.readInt16LE(this.position)
+        this.position += 2
+        return ret
+    }
 
-	int32() {
-		const ret = this.buffer.readInt32LE(this.position)
-		this.position += 4
-		return ret
-	}
+    int32() {
+        const ret = this.buffer.readInt32LE(this.position)
+        this.position += 4
+        return ret
+    }
 
-	int64() {
-		return BigInt(this.uint32()) | (BigInt(this.int32()) << 32n)
-	}
+    int64() {
+        return BigInt(this.uint32()) | (BigInt(this.int32()) << 32n)
+    }
 
-	vec3() {
-		return new Vec3(this.float(), this.float(), this.float())
-	}
+    vec3() {
+        return new Vec3(this.float(), this.float(), this.float())
+    }
 
-	vec3fa() {
-		return new Vec3(this.float() * MULT_INT16_TO_RAD, this.float() * MULT_INT16_TO_RAD, this.float() * MULT_INT16_TO_RAD)
-	}
+    vec3fa() {
+        return new Vec3(this.float() * MULT_INT16_TO_RAD, this.float() * MULT_INT16_TO_RAD, this.float() * MULT_INT16_TO_RAD)
+    }
 
-	angle() {
-		return this.int16() * MULT_INT16_TO_RAD
-	}
+    angle() {
+        return this.int16() * MULT_INT16_TO_RAD
+    }
 
-	skillid() {
-		const raw = this.uint64(),
-			type = Number((raw >> 28n) & 0xfn),
-            npc = Boolean(raw & 0x0100000000n)
+    skillid() {
+        const raw = this.uint64(),
+            type = Number((raw >> 28n) & 0xfn),
+            npc = Boolean(raw & 0x0100000000n),
+            hasHuntingZone = npc && type === 1
 
-		return new SkillID({
-            id: Number(raw & (npc ? 0xffffn : 0xfffffffn)),
-			huntingZoneId: npc ? Number((raw >> 16n) & 0xfffn) : 0,
-			type,
-			npc,
-			reserved: Number(raw >> 33n)
-		})
-	}
+        return new SkillID({
+            id: Number(raw & (hasHuntingZone ? 0xffffn : 0xfffffffn)),
+            huntingZoneId: hasHuntingZone ? Number((raw >> 16n) & 0xfffn) : 0,
+            type,
+            npc,
+            reserved: Number(raw >> 33n)
+        })
+    }
 
     customize() {
         return new Customize(this.uint64());
     }
 
-	float() {
-		const ret = this.buffer.readFloatLE(this.position)
-		this.position += 4
-		return ret
-	}
+    float() {
+        const ret = this.buffer.readFloatLE(this.position)
+        this.position += 4
+        return ret
+    }
 
-	double() {
-		const ret = this.buffer.readDoubleLE(this.position)
-		this.position += 8
-		return ret
-	}
+    double() {
+        const ret = this.buffer.readDoubleLE(this.position)
+        this.position += 8
+        return ret
+    }
 
-	string() {
-		const ret = []
-		let c, i = -1
-		while(c = this.uint16()) ret[++i] = c
-		return String.fromCharCode.apply(null, ret)
-	}
+    string() {
+        const ret = []
+        let c, i = -1
+        while(c = this.uint16()) ret[++i] = c
+        return String.fromCharCode.apply(null, ret)
+    }
 }
 
 class Writeable {
-	constructor(length) {
-		this.length = length
-		this.buffer = Buffer.alloc(this.length)
-		this.position = 0
-	}
+    constructor(length) {
+        this.length = length
+        this.buffer = Buffer.alloc(this.length)
+        this.position = 0
+    }
 
-	seek(n) { this.position = n }
-	skip(n) { this.position += n }
+    seek(n) { this.position = n }
+    skip(n) { this.position += n }
 
-	bool(b) { this.buffer[this.position++] = !!b }
-	byte(n) { this.buffer[this.position++] = n }
-	bytes(buf) {
-		if(buf) {
-			buf.copy(this.buffer, this.position)
-			this.position += buf.length
-		}
-	}
-	uint16(n = 0) { this.position = this.buffer.writeUInt16LE(n & 0xffff, this.position) }
-	uint32(n = 0) { this.position = this.buffer.writeUInt32LE(n >>> 0, this.position) }
-	uint64(n = 0n) {
+    bool(b) { this.buffer[this.position++] = !!b }
+    byte(n) { this.buffer[this.position++] = n }
+    bytes(buf) {
+        if(buf) {
+            buf.copy(this.buffer, this.position)
+            this.position += buf.length
+        }
+    }
+    uint16(n = 0) { this.position = this.buffer.writeUInt16LE(n & 0xffff, this.position) }
+    uint32(n = 0) { this.position = this.buffer.writeUInt32LE(n >>> 0, this.position) }
+    uint64(n = 0n) {
         if(typeof n === 'number')
             n = BigInt(n)
         this.uint32(Number(n & 0xffffffffn))
-		this.uint32(Number((n >> 32n) & 0xffffffffn))
-	}
-	vec3(v = {}) {
-		this.float(v.x)
-		this.float(v.y)
-		this.float(v.z)
-	}
-	vec3fa(v = {}) {
-		this.float(Math.round(v.x * MULT_RAD_TO_INT16 % 0x10000))
-		this.float(Math.round(v.y * MULT_RAD_TO_INT16 % 0x10000))
-		this.float(Math.round(v.z * MULT_RAD_TO_INT16 % 0x10000))
-	}
-	angle(r = 0) { this.int16(Math.round(r * MULT_RAD_TO_INT16)) }
+        this.uint32(Number((n >> 32n) & 0xffffffffn))
+    }
+    vec3(v = {}) {
+        this.float(v.x)
+        this.float(v.y)
+        this.float(v.z)
+    }
+    vec3fa(v = {}) {
+        this.float(Math.round(v.x * MULT_RAD_TO_INT16 % 0x10000))
+        this.float(Math.round(v.y * MULT_RAD_TO_INT16 % 0x10000))
+        this.float(Math.round(v.z * MULT_RAD_TO_INT16 % 0x10000))
+    }
+    angle(r = 0) { this.int16(Math.round(r * MULT_RAD_TO_INT16)) }
 
-	skillid(obj = {}) {
+    skillid(obj = {}) {
         if(typeof obj === 'number')
             obj = {type: 1, id: obj}
 
-		let raw = BigInt((Number(obj.id) || 0) & (obj.npc ? 0xffff : 0xfffffff))
-		if (obj.npc)
+        const hasHuntingZone = Boolean(obj.npc) && obj.type === 1
+        
+        let raw = BigInt((Number(obj.id) || 0) & (hasHuntingZone ? 0xffff : 0xfffffff))
+        if (hasHuntingZone)
             raw |= BigInt(obj.huntingZoneId & 0xfff) << 16n
-		raw |= BigInt(obj.type & 0xf) << 28n
-		raw |= BigInt(obj.npc & 1) << 32n
-		raw |= BigInt(obj.reserved & 1) << 33n
+        raw |= BigInt(obj.type & 0xf) << 28n
+        raw |= BigInt(obj.npc & 1) << 32n
+        raw |= BigInt(obj.reserved & 1) << 33n
 
-		this.uint64(raw)
-	}
+        this.uint64(raw)
+    }
 
     customize(val) {
         switch(typeof val) {
@@ -176,18 +179,18 @@ class Writeable {
         }
     }
 
-	float(n = 0) { this.position = this.buffer.writeFloatLE(n, this.position) }
-	double(n = 0) { this.position = this.buffer.writeDoubleLE(n, this.position) }
-	string(str = '') { this.buffer.fill(str + '\0', this.position, this.position += (str.length + 1) * 2, 'ucs2') }
+    float(n = 0) { this.position = this.buffer.writeFloatLE(n, this.position) }
+    double(n = 0) { this.position = this.buffer.writeDoubleLE(n, this.position) }
+    string(str = '') { this.buffer.fill(str + '\0', this.position, this.position += (str.length + 1) * 2, 'ucs2') }
 }
 
 Object.assign(Writeable.prototype, {
-	int16: Writeable.prototype.uint16,
-	int32: Writeable.prototype.uint32,
-	int64: Writeable.prototype.uint64
+    int16: Writeable.prototype.uint16,
+    int32: Writeable.prototype.uint32,
+    int64: Writeable.prototype.uint64
 })
 
 module.exports = {
-	Readable,
-	Writeable
+    Readable,
+    Writeable
 }
--- node_modules/tera-data-parser/lib/protocol/types/skillid.js
+++ node_modules/tera-data-parser/lib/protocol/types/skillid.js
@@ -1,45 +1,46 @@
 'use strict'
 
 function parseArgs(obj) {
-	if(typeof obj === 'number') obj = {type: 1, id: obj}
-	else if(obj == null) obj = {}
+    if(typeof obj === 'number') obj = {type: 1, id: obj}
+    else if(obj == null) obj = {}
 
-	const npc = Boolean(obj.npc),
-		type = obj.type & 0xf
+    const npc = Boolean(obj.npc),
+        type = obj.type & 0xf,
+        hasHuntingZone = npc && type === 1
 
-	return {
-		reserved: obj.reserved & 0x7fffffff,
-		npc,
-		type,
-		huntingZoneId: npc ? obj.huntingZoneId & 0xfff : 0,
-        id: obj.id & (npc ? 0xffff : 0xfffffff)
-	}
+    return {
+        reserved: obj.reserved & 0x7fffffff,
+        npc,
+        type,
+        huntingZoneId: hasHuntingZone ? obj.huntingZoneId & 0xfff : 0,
+        id: obj.id & (hasHuntingZone ? 0xffff : 0xfffffff)
+    }
 }
 
 class SkillID {
-	constructor(obj) { Object.assign(this, parseArgs(obj)) }
+    constructor(obj) { Object.assign(this, parseArgs(obj)) }
 
-	equals(obj) {
-		obj = parseArgs(obj)
+    equals(obj) {
+        obj = parseArgs(obj)
 
-		return this.id === obj.id && this.huntingZoneId === obj.huntingZoneId &&
-			this.type === obj.type && this.npc === obj.npc && this.reserved === obj.reserved
-	}
+        return this.id === obj.id && this.huntingZoneId === obj.huntingZoneId &&
+            this.type === obj.type && this.npc === obj.npc && this.reserved === obj.reserved
+    }
 
-	clone() { return Object.assign(Object.create(SkillID.prototype), this) }
+    clone() { return Object.assign(Object.create(SkillID.prototype), this) }
 
-	toString() {
-		let str = this.reserved ? `[X${this.reserved.toString(16)}]` : ''
+    toString() {
+        let str = this.reserved ? `[X${this.reserved.toString(16)}]` : ''
 
-		switch(this.type) {
-			case 1: str += 'A'; break
-			case 2: str += 'R'; break
-			default: str += `[T${this.type}]`; break
-		}
+        switch(this.type) {
+            case 1: str += 'A'; break
+            case 2: str += 'R'; break
+            default: str += `[T${this.type}]`; break
+        }
 
-		if(this.npc) str += `${this.huntingZoneId}:`
-		return str + this.id
-	}
+        if(this.npc && this.type === 1) str += `${this.huntingZoneId}:`
+        return str + this.id
+    }
 }
 
 module.exports = SkillID
