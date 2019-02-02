9e952cbca91c415b6ec5cdc10f5146558139a199
Caali <modsfortera@gmail.com>
2019-02-02 12:45:06 +0100

    Preparation for future patch.

--- bin/index.js
+++ bin/index.js
@@ -1,20 +1,26 @@
 async function updateSelf() {
-  delete require.cache[require.resolve('./update-self')];
-  const autoUpdateSelf = require("./update-self");
-  try {
-    let result = await autoUpdateSelf();
-    if(result)
-      return updateSelf();
-    else
-      return true;
-  } catch(_) {
-    return false;
-  }
+    delete require.cache[require.resolve('./update-self')];
+    const autoUpdateSelf = require("./update-self");
+    try {
+        let result = await autoUpdateSelf();
+        if (result)
+            return updateSelf();
+        else
+            return true;
+    } catch (_) {
+        return false;
+    }
 }
 
 updateSelf().then((result) => {
-  if(result)
-    require("./loader");
-  else
-    console.log("Failed to auto-update the proxy, terminating...");
+    if (result) {
+        try {
+            // Preparation for smooth migration to new boot sequence
+            require(process.versions.electron ? './index-gui' : './index-cli');
+        } catch (_) {
+            require("./loader");
+        }
+    } else {
+        console.log("Failed to auto-update the proxy, terminating...");
+    }
 });
