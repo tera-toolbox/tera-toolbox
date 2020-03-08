9782291196e958a486c7bc03d81637c27614d266
Caali <modsfortera@gmail.com>
2020-03-08 01:35:02 +0100

    Update standalone injector.

--- bin/standalone-injector.js
+++ bin/standalone-injector.js
@@ -1,5 +1,7 @@
-﻿const ProcessListenerDLLInjector = require('tera-client-interface/process-listener-dll-injector');
+﻿const path = require('path');
+const Scanner = require('tera-client-interface/scanner');
 
-let processListener = new ProcessListenerDLLInjector(100);
-processListener.run();
+let scanner = new Scanner('TERA.exe', path.join(path.dirname(require.resolve('tera-client-interface')), 'tera-client-interface.dll'), 25);
+scanner.start();
 console.log('[toolbox] Standalone DLL injector running!');
+process.stdin.resume();
