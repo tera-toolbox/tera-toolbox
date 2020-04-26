80c947fbd077c41bec5a4079fb9788e36e29d2e3
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2020-04-27 00:27:23 +0300

    arabian translation update, migration

--- bin/migration.js
+++ bin/migration.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const fs = require("fs");
-const { rimraf } = require('./utils');
+const { rimraf } = require("./utils");
 
 function tryUnlink(file) {
     try {
@@ -13,106 +13,107 @@ function tryUnlink(file) {
 // Migrate from old versions
 function ToolboxMigration() {
     // Delete legacy servers folder
-    rimraf(path.join(__dirname, 'servers'));
+    rimraf(path.join(__dirname, "servers"));
 
     // Delete legacy node_modules folders
     const DeprecatedNodeModules = [
-        '.bin',
-        'ajv',
-        'asn1',
-        'assert-plus',
-        'asynckit',
-        'aws-sign2',
-        'aws4',
-        'balanced-match',
-        'baldera-logger',
-        'bcrypt-pbkdf',
-        'boom',
-        'brace-expansion',
-        'bunyan',
-        'caseless',
-        'co',
-        'combined-stream',
-        'concat-map',
-        'core-util-is',
-        'cryptiles',
-        'dashdash',
-        'delayed-stream',
-        'dtrace-provider',
-        'ecc-jsbn',
-        'eventemitter3',
-        'extend',
-        'extsprintf',
-        'fast-deep-equal',
-        'fast-json-stable-stringify',
-        'forever-agent',
-        'form-data',
-        'getpass',
-        'glob',
-        'har-schema',
-        'har-validator',
-        'hawk',
-        'hoek',
-        'http-proxy',
-        'http-signature',
-        'inflight',
-        'inherits',
-        'is-typedarray',
-        'isstream',
-        'jsbn',
-        'json-schema',
-        'json-schema-traverse',
-        'json-stringify-safe',
-        'jsprim',
-        'lodash',
-        'long',
-        'mime-db',
-        'mime-types',
-        'minimatch',
-        'minimist',
-        'mkdirp',
-        'moment',
-        'mv',
-        'nan',
-        'ncp',
-        'node-stream-zip',
-        'oauth-sign',
-        'once',
-        'path-is-absolute',
-        'performance-now',
-        'pify',
-        'process-list',
-        'punycode',
-        'qs',
-        'request',
-        'request-promise-core',
-        'request-promise-native',
-        'requires-port',
-        'rimraf',
-        'safe-buffer',
-        'safe-json-stringify',
-        'sntp',
-        'sshpk',
-        'stealthy-require',
-        'stringstream',
-        'tera-crypto-js',
-        'tera-crypto-native',
-        'tera-proxy-game',
-        'tera-proxy-sls',
-        'tough-cookie',
-        'tunnel-agent',
-        'tweetnacl',
-        'uuid',
-        'verror',
-        'wrappy',
-        'xmldom'
+        ".bin",
+        "ajv",
+        "asn1",
+        "assert-plus",
+        "asynckit",
+        "aws-sign2",
+        "aws4",
+        "balanced-match",
+        "baldera-logger",
+        "bcrypt-pbkdf",
+        "boom",
+        "brace-expansion",
+        "bunyan",
+        "caseless",
+        "co",
+        "combined-stream",
+        "concat-map",
+        "core-util-is",
+        "cryptiles",
+        "dashdash",
+        "delayed-stream",
+        "dtrace-provider",
+        "ecc-jsbn",
+        "eventemitter3",
+        "extend",
+        "extsprintf",
+        "fast-deep-equal",
+        "fast-json-stable-stringify",
+        "forever-agent",
+        "form-data",
+        "getpass",
+        "glob",
+        "har-schema",
+        "har-validator",
+        "hawk",
+        "hoek",
+        "http-proxy",
+        "http-signature",
+        "inflight",
+        "inherits",
+        "is-typedarray",
+        "isstream",
+        "jsbn",
+        "json-schema",
+        "json-schema-traverse",
+        "json-stringify-safe",
+        "jsprim",
+        "lodash",
+        "long",
+        "mime-db",
+        "mime-types",
+        "minimatch",
+        "minimist",
+        "mkdirp",
+        "moment",
+        "mv",
+        "nan",
+        "ncp",
+        "node-stream-zip",
+        "oauth-sign",
+        "once",
+        "path-is-absolute",
+        "performance-now",
+        "pify",
+        "process-list",
+        "punycode",
+        "qs",
+        "request",
+        "request-promise-core",
+        "request-promise-native",
+        "requires-port",
+        "rimraf",
+        "safe-buffer",
+        "safe-json-stringify",
+        "sntp",
+        "sshpk",
+        "stealthy-require",
+        "stringstream",
+        "tera-crypto-js",
+        "tera-crypto-native",
+        "tera-proxy-game",
+        "tera-proxy-sls",
+        "tough-cookie",
+        "tunnel-agent",
+        "tweetnacl",
+        "uuid",
+        "verror",
+        "wrappy",
+        "xmldom"
     ];
 
     for (const NodeModule of DeprecatedNodeModules)
-        rimraf(path.join(__dirname, '..', 'node_modules', NodeModule));
-    rimraf(path.join(__dirname, '..', 'node_modules', 'tera-data-parser', 'lib', 'sysmsg'));
-    rimraf(path.join(__dirname, '..', 'node_modules', 'tera-network-crypto', 'bin', 'ia32'));
-    rimraf(path.join(__dirname, '..', 'node_modules', 'tera-network-crypto', 'bin', 'x64'));
+        rimraf(path.join(__dirname, "..", "node_modules", NodeModule));
+    rimraf(path.join(__dirname, "..", "node_modules", "tera-data-parser", "lib", "sysmsg"));
+    rimraf(path.join(__dirname, "..", "node_modules", "tera-network-crypto", "bin", "ia32"));
+    rimraf(path.join(__dirname, "..", "node_modules", "tera-network-crypto", "bin", "x64"));
+    rimraf(path.join(__dirname, "gui", "icons"));
 
     // Delete no longer used files
     tryUnlink(path.join(__dirname, "index.js"));
@@ -123,10 +124,20 @@ function ToolboxMigration() {
     tryUnlink(path.join(__dirname, "regions.js"));
     tryUnlink(path.join(__dirname, "update-electron-helper.js"));
     tryUnlink(path.join(__dirname, "gui", "index.html"));
-    tryUnlink(path.join(__dirname, '..', 'node_modules', 'tera-data-parser', 'lib', 'protocol', 'stream.js'));
-    tryUnlink(path.join(__dirname, '..', 'node_modules', 'tera-client-interface', 'injector.exe'));
-    tryUnlink(path.join(__dirname, '..', 'node_modules', 'tera-client-interface', 'process-listener.js'));
-    tryUnlink(path.join(__dirname, '..', 'node_modules', 'tera-client-interface', 'process-listener-dll-injector.js'));
+    tryUnlink(path.join(__dirname, "gui", "icon.ico"));
+    tryUnlink(path.join(__dirname, "gui", "icon_xmas.ico"));
+    tryUnlink(path.join(__dirname, "gui", "main.js"));
+    tryUnlink(path.join(__dirname, "gui", "splash.js"));
+    tryUnlink(path.join(__dirname, "gui", "splash.mp4"));
+    tryUnlink(path.join(__dirname, "gui", "splash_xmas.mp4"));
+    tryUnlink(path.join(__dirname, "gui", "js", "jquery-3.2.1.min.js"));
+    tryUnlink(path.join(__dirname, "gui", "js", "jquery-3.4.1.min.js"));
+    tryUnlink(path.join(__dirname, "..", "node_modules", "tera-mod-ui", "settings_ui", "icon.png"));
+    tryUnlink(path.join(__dirname, "..", "node_modules", "tera-mod-ui", "settings_ui", "jquery-3.2.1.min.js"));
+    tryUnlink(path.join(__dirname, "..", "node_modules", "tera-data-parser", "lib", "protocol", "stream.js"));
+    tryUnlink(path.join(__dirname, "..", "node_modules", "tera-client-interface", "injector.exe"));
+    tryUnlink(path.join(__dirname, "..", "node_modules", "tera-client-interface", "process-listener.js"));
+    tryUnlink(path.join(__dirname, "..", "node_modules", "tera-client-interface", "process-listener-dll-injector.js"));
 }
 
 module.exports = { ToolboxMigration };
