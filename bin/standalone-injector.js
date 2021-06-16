f1f538c64ac02b0ade5a7d8d584fb0389bc0532b
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2021-06-16 09:46:14 +0300

    remove standalone injector

--- bin/standalone-injector.js
+++
@@ -1,7 +0,0 @@
-﻿const path = require('path');
-const Scanner = require('tera-client-interface/scanner');
-
-let scanner = new Scanner('TERA.exe', path.join(path.dirname(require.resolve('tera-client-interface')), 'tera-client-interface-32.dll'), path.join(path.dirname(require.resolve('tera-client-interface')), 'tera-client-interface-64.dll'), 25);
-scanner.start();
-console.log('[toolbox] Standalone DLL injector running!');
-process.stdin.resume();
